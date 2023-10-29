467502d3b3b48fc33df8961c9f848c1edb38a255
Yasar Cayir <yasar.cayir@hotmail.com>
2023-10-29 14:51:23 +0100

    implementing template forms for the login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: ''
+  }
 
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
+
+  login() {
+    console.log(this.credentials)
+    this.showAlert = true;
+    this.alertMsg = 'You are succesfully logged in.';
+    this.alertColor = 'blue';
+  }
 }
